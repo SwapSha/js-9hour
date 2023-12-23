2e0c738009af55ae0664eaaa0545558af48eb86c
Swapnil Sharma <137091498+SwapSha@users.noreply.github.com>
2023-12-23 06:47:20 +0000

    DataTypes and practise

---
+++ 01_Basics/Basic02.js
@@ -0,0 +1,21 @@
+"use strict"; // All Code are Written in the New Version of JS
+
+// console.log("Hello "); console.log("World !!");
+
+/*
+console.log( 3 + "3"); // 33
+console.log( 3 - "3"); // 0
+console.log( "3" * 3); // 9
+console.log( "3" / 3); // 1
+console.log( "3" + 3); // 33
+console.log( "3" + null); // NAN -> 3null
+console.log( 3 + null); // NAN -> 3
+console.log( 3 + undefined); // NAN
+console.log( "3" + undefined); // NAN -> 3undefined
+
+null => standalone value
+undefined => Value is not assigned
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
