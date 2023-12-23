a93723a3305fc06d4cd64cb67b56ffbcbf923182
Swapnil Sharma <137091498+SwapSha@users.noreply.github.com>
2023-12-23 07:51:14 +0000

    Moving to Array

---
+++ 01_Basics/Basic03.js
@@ -0,0 +1,19 @@
+// Conversion Operation
+
+let nums = 0
+
+let BoolValue = Boolean(nums)
+
+// console.log(BoolValue);
+
+let value = "20null"
+
+let converse = Number(value)
+
+// console.log(typeof converse);
+
+// console.log("2" + 2 + 2);
+// console.log(3 + 2 + "2");
+// console.log("20" + 10 + "1");
+// console.log(true++);
+console.log(+true);
